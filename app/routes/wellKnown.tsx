e07c65e314d80660d8be7f1bc188cb65a1b345f7
Akash504-ai <santraakash999@gmail.com>
2025-09-07 02:17:51 +0530

    first commit

---
+++ app/routes/wellKnown.tsx
@@ -0,0 +1,6 @@
+export default function WellKnown() {
+  // Return an empty JSON response or message
+  return (
+    <pre>{JSON.stringify({ message: "Chrome DevTools check ignored" })}</pre>
+  );
+}
