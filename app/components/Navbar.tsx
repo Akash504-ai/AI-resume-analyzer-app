1cf90fe669740d78ad33b6a186ca3da49db23d61
Akash504-ai <santraakash999@gmail.com>
2025-09-07 15:10:37 +0530

    second commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,25 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <Link to="/">
-                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
-            </Link>
-            <Link to="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
-        </nav>
-    )
-}
-export default Navbar
+  return (
+    <nav className="w-full top-0 left-0 z-50 bg-transparent backdrop-blur-md">
+      <div className="max-w-7xl mx-auto px-8 py-4 flex items-center justify-between">
+        
+        {/* Logo / Title */}
+        <h1 className="text-2xl font-extrabold text-gray-900 tracking-wide">
+          AI <span className="text-orange-500">Resume</span> Analyzer
+        </h1>
+
+        {/* Upload Button */}
+        <Link
+          to="/upload"
+          className="w-fit flex items-center gap-2 px-5 py-2 rounded-full bg-orange-500 text-white font-medium shadow-lg hover:bg-orange-600 hover:scale-105 transition-all duration-300"
+        >
+          Upload Resume
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -80,18 +80,19 @@ const Upload = () => {
 
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
             <Navbar />
 
             <section className="main-section">
-                <div className="page-heading py-16">
-                    <h1>Smart feedback for your dream job</h1>
+                <div className="page-heading py-15 px-10">
+                    <h1>Smart <span className="text-orange-500">Feedback</span> for your dream job</h1>
                     {isProcessing ? (
                         <>
                             <h2>{statusText}</h2>
                             <img src="/images/resume-scan.gif" className="w-full" />
                         </>
                     ) : (
-                        <h2>Drop your resume for an ATS score and improvement tips</h2>
+                        <h2>Drop your resume for an <span className="text-orange-500">ATS score</span> and improvement tips</h2>
                     )}
                     {!isProcessing && (
                         <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
